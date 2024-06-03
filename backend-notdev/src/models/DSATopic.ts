5490247a5a711d767a3f318776064af110bed527
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-06-03 16:46:13 +0530

    just-for-now

--- backend-notdev/src/models/DSATopic.ts
+++ backend-notdev/src/models/DSATopic.ts
@@ -2,7 +2,7 @@ import mongoose, { Document, Schema } from 'mongoose';
 import IDSATopic from '../interfaces/dsatopic.interface';
 const DSATopicSchema: Schema = new Schema({
   title: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String },
   questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
   totalQuestions: { type: Number, default: 0 },
   easy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
