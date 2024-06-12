ada85ea18f68ed0fb6cb7190c81b64eb9aa1e510
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-06-12 12:31:19 +0530

    chore: Update Firebase configuration for Google Sign In and authentication

--- backend-notdev/src/firebase.ts
+++ backend-notdev/src/firebase.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
-// const serviceAccount = require("./notedev-ca409-firebase-adminsdk-wh8mj-f01a6ede4b");
 const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY || "{}");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
