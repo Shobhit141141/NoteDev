b2e733437faa23ab19ba109acac2db9af1f39ff5
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-06-23 15:38:01 +0530

    chore: Add loading indicator to ProtectedRoute component

--- frontend-notdev/src/components-notdev/ProtectedRoute.tsx
+++ frontend-notdev/src/components-notdev/ProtectedRoute.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { useLocation, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/GoogleAuthContext';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useLocation, Outlet } from "react-router-dom";
+import { useAuth } from "../context/GoogleAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute: React.FC = () => {
-  const { user } = useAuth();
+  const { user, userLoading } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
 
-  if (!user) {
-    navigate('/signin', { state: { from: location } });
-    return null; 
+  if (userLoading) {
+    return (
+      <div className="w-[100vw] h-[100vh] fixed top-0 left-0 flex justify-center items-center bg-[#00000080] bg-opacity-50 z-50">
+        <span className="loading loading-dots loading-sm text-white"></span>
+      </div>
+    );
   }
 
-  return <Outlet />; 
+  if (!user) {
+    navigate("/signin", { state: { from: location } });
+    return null;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
-
-
