1c2f9ac93cb3f2fc41320aa5b3853c78f819a17d
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-07-18 12:52:58 +0530

    Revert "chore: Update GoogleAuthMiddleware to handle access token verification"
    This reverts commit 19548c2fe32f4a739d20bffbf1769f144bdc5922.

--- backend-notdev/src/middleware/GoogleAuthMiddleware.ts
+++ backend-notdev/src/middleware/GoogleAuthMiddleware.ts
@@ -1,28 +1,24 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   // const authHeader = req.headers['authorization'];
-  const frontendUid = req.headers["uid"]; // Assuming the frontend sends the uid in headers
+  const frontendUid = req.headers['uid']; // Assuming the frontend sends the uid in headers
 
   // if (!authHeader) {
   //   return res.status(401).json({ error: 'Authorization header is missing' });
   // }
 
   const accessToken = req.cookies.token;
-  console.log(accessToken);
   try {
     // Verify access token with Google OAuth server
-    const response = await fetch(
-      "https://www.googleapis.com/oauth2/v3/tokeninfo",
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const response = await fetch('https://www.googleapis.com/oauth2/v3/tokeninfo', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
     if (!response.ok) {
-      return res.status(401).json({ error: "Invalid access token" });
+      return res.status(401).json({ error: 'Invalid access token' });
     }
     // @ts-ignore
     const tokenInfo = await response.json();
@@ -30,13 +26,13 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const tokenUid = tokenInfo.sub;
 
     if (tokenUid !== frontendUid) {
-      return res.status(401).json({ error: "Unauthorized: UID mismatch" });
+      return res.status(401).json({ error: 'Unauthorized: UID mismatch' });
     }
 
     next();
   } catch (err) {
-    console.error("Error verifying access token:", err);
-    return res.status(500).json({ error: "Failed to verify access token" });
+    console.error('Error verifying access token:', err);
+    return res.status(500).json({ error: 'Failed to verify access token' });
   }
 };
 
