663401feb2f00e767c674f35cd2415b050eafe67
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-07-18 12:52:43 +0530

    Revert "chore: Add csurf package for CSRF protection in backend-notdev/package.json"
    This reverts commit 79d0a2ab98787dcf827b48a09649319225907224.

--- backend-notdev/index.ts
+++ backend-notdev/index.ts
@@ -1,16 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import bodyParser from "body-parser";
-import path from "path";
-import fetch from "node-fetch";
-import cookieParser from "cookie-parser";
-import User from "./src/models/User";
-import connectDB from "./src/config/db";
-import questionRoutes from "./src/routes/questionRoutes";
-import dsaTopicRoutes from "./src/routes/dsaTopicRoutes";
-import userRoutes from "./src/routes/userRoutes";
-import csrf from "csurf"; // Import the 'csrf' module
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+import fetch from 'node-fetch';
+import cookieParser from 'cookie-parser';
+import User from './src/models/User';
+import connectDB from './src/config/db';
+import questionRoutes from './src/routes/questionRoutes';
+import dsaTopicRoutes from './src/routes/dsaTopicRoutes';
+import userRoutes from './src/routes/userRoutes';
 
 dotenv.config();
 
@@ -19,52 +18,42 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-const csrfProtection = csrf({
-  cookie: {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-  },
-});
-
-app.set("trust proxy", 1);
-
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Allow only your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 app.use(cookieParser()); // Add cookie parser middleware
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI =
-  process.env.REDIRECT_URI || "http://localhost:5000/auth/google/callback";
+const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:5000/auth/google/callback';
 
-app.get("/auth/google", (req, res) => {
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(
-    REDIRECT_URI
-  )}&scope=profile email`;
+app.get('/auth/google', (req, res) => {
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=profile email`;
   res.redirect(authUrl);
 });
 
-app.get("/auth/google/callback", async (req, res) => {
+app.get('/auth/google/callback', async (req, res) => {
   const { code } = req.query;
-  if (!code || typeof code !== "string") {
-    return res.status(400).json({ error: "Invalid code parameter" });
+  if (!code || typeof code !== 'string') {
+    return res.status(400).json({ error: 'Invalid code parameter' });
   }
 
   try {
-    const response = await fetch("https://oauth2.googleapis.com/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    const response = await fetch('https://oauth2.googleapis.com/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams({
         code,
         client_id: GOOGLE_CLIENT_ID!,
         client_secret: GOOGLE_CLIENT_SECRET!,
         redirect_uri: REDIRECT_URI!,
-        grant_type: "authorization_code",
+        grant_type: 'authorization_code',
       }),
     });
 
@@ -76,12 +65,9 @@ app.get("/auth/google/callback", async (req, res) => {
     const accessToken = data.access_token;
     const expires_in = data.expires_in * 1000;
 
-    const userResponse = await fetch(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      }
-    );
+    const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
 
     const userData = await userResponse.json();
 
@@ -95,66 +81,53 @@ app.get("/auth/google/callback", async (req, res) => {
       });
       await newUser.save();
     }
-    const expiryDate = new Date(Date.now() + expires_in * 1000);
-
+    const expiryDate = new Date(Date.now() + expires_in);
+    
     // Set cookies
-    res.cookie("token", accessToken, {
-      httpOnly: true,
-      secure: true,
-      expires: expiryDate,
-    });
-    res.cookie("uid", userData.id, {
-      httpOnly: true,
-      secure: true,
-      expires: expiryDate,
-    });
+    res.cookie('token', accessToken, { httpOnly: true, secure: true, expires: expiryDate });
+    res.cookie('uid', userData.id, { httpOnly: true, secure: true, expires: expiryDate });
 
-    res.redirect(
-      `${process.env.FRONTEND_URL}/?token=${accessToken}&uid=${userData.id}`
-    );
+    res.redirect(`${process.env.FRONTEND_URL}/?token=${accessToken}&uid=${userData.id}`);
   } catch (err) {
-    console.error("Error fetching access token:", err);
-    res.status(500).json({ error: "Failed to fetch access token" });
+    console.error('Error fetching access token:', err);
+    res.status(500).json({ error: 'Failed to fetch access token' });
   }
 });
 
-app.get("/auth/user/profile", csrfProtection, async (req, res) => {
+app.get('/auth/user/profile', async (req, res) => {
   const token = req.cookies.token;
-  console.log("Token:", token); // Log the token for debugging purposes
+  console.log('Token:', token); // Log the token for debugging purposes
 
   if (!token) {
-    return res.status(401).json({ error: "Authorization token is missing" });
+    return res.status(401).json({ error: 'Authorization token is missing' });
   }
 
   try {
-    const userResponse = await fetch(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+    const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
     if (!userResponse.ok) {
       const errorText = await userResponse.text();
-      console.error("Error response from Google API:", errorText); // Log the error response from Google API
-      throw new Error("Failed to fetch user info");
+      console.error('Error response from Google API:', errorText); // Log the error response from Google API
+      throw new Error('Failed to fetch user info');
     }
 
     const userData = await userResponse.json();
     res.json({ user: userData });
   } catch (err) {
-    console.error("Error fetching user profile:", err);
-    res.status(500).json({ error: "Failed to fetch user profile" });
+    console.error('Error fetching user profile:', err);
+    res.status(500).json({ error: 'Failed to fetch user profile' });
   }
 });
 
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/api/questions", csrfProtection, questionRoutes);
-app.use("/api/topics", csrfProtection, dsaTopicRoutes);
-app.use("/api/users", csrfProtection, userRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/questions', questionRoutes);
+app.use('/api/topics', dsaTopicRoutes);
+app.use('/api/users', userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
 app.listen(PORT, () => {
